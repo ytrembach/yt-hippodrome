fe9591ccf9dc4a9448553540bb8b1025da75df10
Yury Trembach <yury.trembach@gmail.com>
2024-05-04 15:32:04 +0300

    Main logging

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    final private static Logger logger = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -14,6 +18,7 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+        logger.info(String.format("Початок стрибків. Кількість учасників: %d", horses.size()));
 
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
@@ -23,6 +28,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+        logger.info(String.format("Закінчення стрибків. Переможець: %s",hippodrome.getWinner().getName()));
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
